bbf7c95f195e0812f8e3fa40fce4eff95afdd6d9
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-11-19 13:34:09 +0600

    problem 2 solved

--- src/solution.ts
+++ src/solution.ts
@@ -1,17 +1,34 @@
-type FormatValueInputOutputType = string | boolean | number 
+// type FormatValueInputOutputType = string | boolean | number 
 
-const formatValue = (value: FormatValueInputOutputType): FormatValueInputOutputType => {
+// const formatValue = (value: FormatValueInputOutputType): FormatValueInputOutputType => {
+//     if (typeof value === 'string') {
+//         return value.toUpperCase()
+//     } else if (typeof value === 'number') {
+//         return value * 10
+//     } else if (typeof value === 'boolean') {
+//         return !value
+//     } else {
+//         return 'Invalid parameter'
+//     }
+// }
+
+// console.log(formatValue('hello'));
+// console.log(formatValue(5));
+// console.log(formatValue(true));
+
+type GetLengthInputType = string | string[] | object[] | number[] | boolean[] | (string | number | boolean | object)[]
+
+const getLength = (value: GetLengthInputType): number | string => {
     if (typeof value === 'string') {
-        return value.toUpperCase()
-    } else if (typeof value === 'number') {
-        return value * 10
-    } else if (typeof value === 'boolean') {
-        return !value
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
     } else {
-        return 'Invalid parameter'
+        return 'Invalid Parameter'
     }
 }
 
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(true));
+console.log(getLength('typescript'));
+console.log(getLength(['don', 'don', 'don', 'don']));
+console.log(getLength([1, 2, 3, 4]));
+console.log(getLength([{name: 'kabir'}, false, 'false', 20]));
