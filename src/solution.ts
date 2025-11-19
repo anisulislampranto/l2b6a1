9816a0b78e781fa2cc4ecf2a67247c05f1fbc442
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-11-19 14:57:08 +0600

    solution 3

--- src/solution.ts
+++ src/solution.ts
@@ -1,3 +1,4 @@
+// solution 1
 // type FormatValueInputOutputType = string | boolean | number 
 
 // const formatValue = (value: FormatValueInputOutputType): FormatValueInputOutputType => {
@@ -16,19 +17,34 @@
 // console.log(formatValue(5));
 // console.log(formatValue(true));
 
-type GetLengthInputType = string | string[] | object[] | number[] | boolean[] | (string | number | boolean | object)[]
+// solution 2
+// type GetLengthInputType = string | string[] | object[] | number[] | boolean[] | (string | number | boolean | object)[]
 
-const getLength = (value: GetLengthInputType): number | string => {
-    if (typeof value === 'string') {
-        return value.length;
-    } else if (Array.isArray(value)) {
-        return value.length;
-    } else {
-        return 'Invalid Parameter'
+// const getLength = (value: GetLengthInputType): number | string => {
+//     if (typeof value === 'string') {
+//         return value.length;
+//     } else if (Array.isArray(value)) {
+//         return value.length;
+//     } else {
+//         return 'Invalid Parameter'
+//     }
+// }
+
+// console.log(getLength('typescript'));
+// console.log(getLength(['don', 'don', 'don', 'don']));
+// console.log(getLength([1, 2, 3, 4]));
+// console.log(getLength([{name: 'kabir'}, false, 'false', 20]));
+
+// solution 3
+class Person {
+    constructor(public name: string, public age: number){}
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}`
     }
 }
 
-console.log(getLength('typescript'));
-console.log(getLength(['don', 'don', 'don', 'don']));
-console.log(getLength([1, 2, 3, 4]));
-console.log(getLength([{name: 'kabir'}, false, 'false', 20]));
+const person1 = new Person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+console.log(person2.getDetails());
