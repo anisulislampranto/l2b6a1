3f5558b711ed56732938c20a2f9152bfd8d11ada
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-11-19 20:53:56 +0600

    solution 4

--- src/solution.ts
+++ src/solution.ts
@@ -36,15 +36,44 @@
 // console.log(getLength([{name: 'kabir'}, false, 'false', 20]));
 
 // solution 3
-class Person {
-    constructor(public name: string, public age: number){}
-    getDetails(){
-        return `'Name: ${this.name}, Age: ${this.age}'`
+// class Person {
+//     constructor(public name: string, public age: number){}
+//     getDetails(){
+//         return `'Name: ${this.name}, Age: ${this.age}'`
+//     }
+// }
+
+// const person1 = new Person('John Doe', 30);
+// console.log(person1.getDetails());
+
+// const person2 = new Person('Alice', 25);
+// console.log(person2.getDetails());
+
+
+// solution 4 
+type ArrayOfObjects = { title: string, rating: number }[]
+
+const filterByRating = (inputArray: ArrayOfObjects): ArrayOfObjects | string => {
+    if (Array.isArray(inputArray)) {
+        let filteredArray = []
+        for (let index = 0; index < inputArray.length; index++) {
+            const element = inputArray[index];
+            if (element.rating < 5 || element.rating > 0) {
+                if (element.rating > 4) {
+                    filteredArray.push(element)
+                }
+            }
+        }
+        return filteredArray;
     }
+
+    return 'Rating cannot be less then 0 or greater then 5'
 }
 
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
-
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
+const books = [
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
+];
+console.log(books);
+console.log(filterByRating(books));
